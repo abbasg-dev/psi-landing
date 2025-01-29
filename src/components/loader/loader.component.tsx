3d57732bc1e28e2ef4ae7b1e9f004f1e8dd28908
aghaith <abbasghayth5@gmail.com>
2025-01-30 01:36:04 +0400

    Implement loader letter animation

--- src/components/loader/loader.component.tsx
+++ src/components/loader/loader.component.tsx
@@ -4,13 +4,21 @@ import styles from "./loader.module.scss";
 const Loading = () => {
   return (
     <div className={styles.loading}>
+      <video
+        src={assets.loading}
+        className={styles.bckVideo}
+        autoPlay
+        loop
+        muted
+      />
       <img src={assets.layer} alt="layer" className={styles.layerLogo} />
-      <div className="container">
-        <video src={assets.loading} autoPlay loop muted />
-        <div className={styles.middleContent}>
-          <img src={assets.animatedLogo} alt="animated-logo" />
-          <div>Loading...</div>
+      <div className={styles.container}>
+        <div className={styles.animatedLetters}>
+          <img src={assets.iLetter} alt="i-letter" />
+          <img src={assets.pLetter} alt="p-letter" />
+          <img src={assets.sLetter} alt="s-letter" />
         </div>
+        <div className={styles.loadingText}>Loading...</div>
       </div>
     </div>
   );
