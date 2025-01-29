15e5118cf7a9c78efb945409e75f13afb611d07c
aghaith <abbasghayth5@gmail.com>
2025-01-30 00:04:50 +0400

    Fixing styles

--- src/App.tsx
+++ src/App.tsx
@@ -62,7 +62,11 @@ function App() {
         <img
           src={assets.vectary}
           alt="vectary"
-          className={`${showChatBot ? "vectary bot_clicked" : "vectary"}`}
+          className={`${
+            showChatBot && vectaryPosition < 70
+              ? "vectary bot_clicked"
+              : "vectary"
+          }`}
           style={{
             bottom: `${vectaryPosition}%`,
           }}
